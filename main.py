25ef973d745dab5477f1e3ff86b623ccb74f3131
Kbgeptupsi <cristianalarcon109@gmail.com>
2024-01-04 14:57:57 -0500

    Refactor main.py: Import module and add download functionality

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import flet as ft 
-import pytube import Youtube
+from pytube import YouTube
 import os
+
+def main(page):
+    url = ft.TextField(label="url",
+                      placeholder="url",
+                      autofocus=True)
+    submit = ft.ElevatedButton("Descargar")
+
+    def btn_click(e):
+        current_folder =os.getcwd()
+        yt = YouTube(url.value)
+        video = yt.streams.get_highest_resolution()
+        video.download(output_path=current_folder)
+
+
+    submit.on_click(btn_click)
+
+    page.add(
+        url,
+        submit
+    )
+
+    ft.app(target=main)
